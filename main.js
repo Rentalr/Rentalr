2c8264abd6cda744249bd176ff82367ad813933e
viv347 <vkinyua001@gmail.com>
2022-03-11 00:10:45 +0300

    ...

---
+++ main.js
@@ -0,0 +1,19 @@
+//swipper slider
+var swiper = new Swiper(".bg-slider-thumbs", {
+        loop: true,
+        spaceBetween: 10,
+        slidesPerView: 4,
+        freeMode: true,
+        watchSlidesProgress: true,
+      });
+      var swiper2 = new Swiper(".bg-slider", {
+        loop: true,
+        spaceBetween: 10,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        thumbs: {
+          swiper: swiper,
+        },
+      });
