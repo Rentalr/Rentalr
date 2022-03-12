d9db40a21db16bdfed8413b38c3aeac0597d2631
viv347 <vkinyua001@gmail.com>
2022-03-12 07:37:37 +0300

    .....

--- main.js
+++ main.js
@@ -1,19 +1,10 @@
 //swipper slider
-var swiper = new Swiper(".bg-slider-thumbs", {
-        loop: true,
-        spaceBetween: 10,
-        slidesPerView: 4,
-        freeMode: true,
-        watchSlidesProgress: true,
-      });
-      var swiper2 = new Swiper(".bg-slider", {
-        loop: true,
-        spaceBetween: 10,
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-        thumbs: {
-          swiper: swiper,
-        },
-      });
+var counter =1;
+setInterval(function(){
+document.getElementById('radio' +counter).checked =true;
+counter++;
+if(counter >5){
+  counter =1;
+}
+},2000
+)
