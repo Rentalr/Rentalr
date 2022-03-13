1ec39e7e3c735e306e2b2b509695dafe36b94ebb
viv347 <vkinyua001@gmail.com>
2022-03-13 19:04:05 +0300

    ...

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-//swipper slider
-var counter =1;
-setInterval(function(){
-document.getElementById('radio' +counter).checked =true;
-counter++;
-if(counter >5){
-  counter =1;
-}
-},2000
-)
+
+// responsive nav menu toggle
+const menuBtn = document.querySelector(".fa-bars");
+const closeBtn = document.querySelector(".fa-xmark");
+const navigation = document.querySelector(".nav");
+
+
+menuBtn.addEventListener("click", () => {
+    navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () =>{
+    navigation.classList.remove("active");
+})
